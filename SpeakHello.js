de6d47cefbdb75acf6efd9958788ef3e64663deb
Nat <144147079+Deagle2@users.noreply.github.com>
2025-05-29 21:22:16 +0530

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+
+(fucntion (window){
+
+  var speakWord = "Hello";
+  var helloSpeaker = {};
+
+  helloSpeaker.speak = function (name){
+    console.log(speakWord + " " + name);
+};
+
+})(window);
